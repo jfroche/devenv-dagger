394f6cd90bf834b0c5dbaa6252064041d13b2137
Jean-François Roche <jfroche@pyxel.be>
2025-11-28 18:10:55 +0100

    refactor(devenv): simplify config and enable git hooks
    Remove example code and unused configuration options. Enable shellcheck and nixpkgs-fmt git hooks for code quality enforcement.

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,4 @@
-{
-  pkgs,
-  ...
+{ ...
 }:
 
 {
@@ -8,39 +6,6 @@
     ./podman.devenv.nix
     ./dagger.devenv.nix
   ];
-
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
-  # https://devenv.sh/packages/
-  packages = [
-    pkgs.git
-  ];
-
-  # https://devenv.sh/languages/
-
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  # https://devenv.sh/basics/
-  enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
-  '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
 
   # https://devenv.sh/tests/
   enterTest = ''
@@ -53,7 +18,8 @@
   '';
 
   # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
+  git-hooks.hooks = {
+    shellcheck.enable = true;
+    nixpkgs-fmt.enable = true;
+  };
 }
